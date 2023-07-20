0b099799ba5a8923fecf50c8d880c07838b4af56
nemojn <nemo@eliworks.com>
2023-07-19 20:03:27 -0400

    Initial commit. Creates homepage and several placeholder links

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+	echo '<head><link rel="stylesheet" type="text/css" href="style.css"></head>';
+	echo '<pre class="header">
+     ____    ____    _____                         
+    |  _ \  |  _ \  |  ___|  _____   _   _   _____ 
+    | |_> } | |_> } | |___  |  _  | | | | | |  _  |
+    |  _ <  |  _ <  |___  | | |_| | | | | | | |_| |
+    | |_> } | |_> }  ___| | |___  | | |_| | |  ___|
+    |____/  |____/  |_____|     |_| |_____| |_____|
+    </pre>';
+	echo '<br>';
+	echo '<p>1: Page 1</p>';
+	echo '<p>2: Page 2</p>';
+    echo '<p>3: Page 3</p>';
+    echo '<p>4: Page 4</p>';
+?>
---
+++ page2/page2.php
@@ -0,0 +1,6 @@
+<?php
+	echo "<head><link rel='stylesheet' type='text/css' href='style.css'></head>";
+	echo "<h1>Page 2</h1>";
+	echo "<br><br><br><br>";
+	echo "<a href='index.php'>Back to main page</a>";
+?>
